45cf00a66adc6ed1d65fa7d77f9201af506d94f1
Luan Moreira <luanmoreiramaia@gmail.com>
2020-04-14 19:35:10 -0300

    animacoes feitas

--- lib/app/pages/pokemon_detail/pokemon_detail_controller.g.dart
+++ lib/app/pages/pokemon_detail/pokemon_detail_controller.g.dart
@@ -44,6 +44,94 @@ mixin _$PokemonDetailController on _PokemonDetailControllerBase, Store {
     }, _$pageControllerAtom, name: '${_$pageControllerAtom.name}_set');
   }
 
+  final _$posicaoAtualAtom =
+      Atom(name: '_PokemonDetailControllerBase.posicaoAtual');
+
+  @override
+  int get posicaoAtual {
+    _$posicaoAtualAtom.context.enforceReadPolicy(_$posicaoAtualAtom);
+    _$posicaoAtualAtom.reportObserved();
+    return super.posicaoAtual;
+  }
+
+  @override
+  set posicaoAtual(int value) {
+    _$posicaoAtualAtom.context.conditionallyRunInAction(() {
+      super.posicaoAtual = value;
+      _$posicaoAtualAtom.reportChanged();
+    }, _$posicaoAtualAtom, name: '${_$posicaoAtualAtom.name}_set');
+  }
+
+  final _$progressAtom = Atom(name: '_PokemonDetailControllerBase.progress');
+
+  @override
+  double get progress {
+    _$progressAtom.context.enforceReadPolicy(_$progressAtom);
+    _$progressAtom.reportObserved();
+    return super.progress;
+  }
+
+  @override
+  set progress(double value) {
+    _$progressAtom.context.conditionallyRunInAction(() {
+      super.progress = value;
+      _$progressAtom.reportChanged();
+    }, _$progressAtom, name: '${_$progressAtom.name}_set');
+  }
+
+  final _$multipleAtom = Atom(name: '_PokemonDetailControllerBase.multiple');
+
+  @override
+  double get multiple {
+    _$multipleAtom.context.enforceReadPolicy(_$multipleAtom);
+    _$multipleAtom.reportObserved();
+    return super.multiple;
+  }
+
+  @override
+  set multiple(double value) {
+    _$multipleAtom.context.conditionallyRunInAction(() {
+      super.multiple = value;
+      _$multipleAtom.reportChanged();
+    }, _$multipleAtom, name: '${_$multipleAtom.name}_set');
+  }
+
+  final _$opacityAtom = Atom(name: '_PokemonDetailControllerBase.opacity');
+
+  @override
+  double get opacity {
+    _$opacityAtom.context.enforceReadPolicy(_$opacityAtom);
+    _$opacityAtom.reportObserved();
+    return super.opacity;
+  }
+
+  @override
+  set opacity(double value) {
+    _$opacityAtom.context.conditionallyRunInAction(() {
+      super.opacity = value;
+      _$opacityAtom.reportChanged();
+    }, _$opacityAtom, name: '${_$opacityAtom.name}_set');
+  }
+
+  final _$visibilityPageViewAtom =
+      Atom(name: '_PokemonDetailControllerBase.visibilityPageView');
+
+  @override
+  bool get visibilityPageView {
+    _$visibilityPageViewAtom.context
+        .enforceReadPolicy(_$visibilityPageViewAtom);
+    _$visibilityPageViewAtom.reportObserved();
+    return super.visibilityPageView;
+  }
+
+  @override
+  set visibilityPageView(bool value) {
+    _$visibilityPageViewAtom.context.conditionallyRunInAction(() {
+      super.visibilityPageView = value;
+      _$visibilityPageViewAtom.reportChanged();
+    }, _$visibilityPageViewAtom, name: '${_$visibilityPageViewAtom.name}_set');
+  }
+
   final _$_PokemonDetailControllerBaseActionController =
       ActionController(name: '_PokemonDetailControllerBase');
 
@@ -61,7 +149,7 @@ mixin _$PokemonDetailController on _PokemonDetailControllerBase, Store {
   @override
   String toString() {
     final string =
-        'pokemon: ${pokemon.toString()},pageController: ${pageController.toString()}';
+        'pokemon: ${pokemon.toString()},pageController: ${pageController.toString()},posicaoAtual: ${posicaoAtual.toString()},progress: ${progress.toString()},multiple: ${multiple.toString()},opacity: ${opacity.toString()},visibilityPageView: ${visibilityPageView.toString()}';
     return '{$string}';
   }
 }
